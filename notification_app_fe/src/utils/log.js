23d7aa66bc89c42bd802497c2bbb980040d380be
KAVYA2929 <144532671+KAVYA2929@users.noreply.github.com>
2026-05-02 12:41:09 +0530

    Final submission - Stage 1 & Stage 2

--- notification_app_fe/src/utils/log.js
+++ notification_app_fe/src/utils/log.js
@@ -1,6 +1,8 @@
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJrYTQyNjBAc3JtaXN0LmVkdS5pbiIsImV4cCI6MTc3NzcwNjA0NCwiaWF0IjoxNzc3NzA1MTQ0LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiMTE2NjhiMmYtODQwYS00ZjZmLWFlZjMtMjIzY2JiNWQ5MzAyIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoia2F2eWEgYWdyYXdhbCIsInN1YiI6IjA5ZjE4ZmExLTZkMWUtNDE2Mi04YjY5LWQwNTM0NTQyMjU1NCJ9LCJlbWFpbCI6ImthNDI2MEBzcm1pc3QuZWR1LmluIiwibmFtZSI6ImthdnlhIGFncmF3YWwiLCJyb2xsTm8iOiJyYTIzMTEwNTMwMTAxMzUiLCJhY2Nlc3NDb2RlIjoiUWticHhIIiwiY2xpZW50SUQiOiIwOWYxOGZhMS02ZDFlLTQxNjItOGI2OS1kMDUzNDU0MjI1NTQiLCJjbGllbnRTZWNyZXQiOiJGam1SYnZwdkRLSGZja3N3In0.XQbCcEPlqPVlsAIxj_qGDtBaJa2s_rdE-O8cq2tB3q8";
 
 const Log = async (stack, level, pkg, message) => {
+  console.log("Sending log:", { stack, level, pkg, message }); // ✅ IMPORTANT
+
   try {
     await fetch("http://20.207.122.201/evaluation-service/logs", {
       method: "POST",
